78c81018f77ec025e1d8942e10361c8adaad004e
SanyFaysal <tfoysalahmed@gmail.com>
2023-10-27 11:08:53 +0600

    [SET UP]: Project structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <input type="text" />
+      <p>Add</p>
+    </>
+  );
+}
+
+export default App;
